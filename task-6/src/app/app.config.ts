bfc34190bb490c2994368aad430a5f4dc3550c74
حبيبة مصطفى محمود قناوى <143437900+Habibakenawy@users.noreply.github.com>
2024-08-10 15:54:56 +0300

    Update app.config.ts

--- task-6/src/app/app.config.ts
+++ task-6/src/app/app.config.ts
@@ -4,6 +4,11 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 
+import { provideHttpClient } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [provideRouter(routes), provideClientHydration(),provideHttpClient(),importProvidersFrom(FormsModule,ReactiveFormsModule),importProvidersFrom(BrowserAnimationsModule)]
 };
+
